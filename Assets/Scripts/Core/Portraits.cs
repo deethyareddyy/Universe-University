493cde416ca28f35a6a17d768c1f13be774b16c3
Sophia <149915043+sopwared@users.noreply.github.com>
2026-04-11 01:47:20 -0500

    random sprite on generate student implemented

--- Assets/Scripts/Core/Portraits.cs
+++ Assets/Scripts/Core/Portraits.cs
@@ -1,6 +1,12 @@
-using System.Collections.Generic;
+using UnityEngine;
 
-public static class Portraits
+
+public class Portraits : MonoBehaviour
 {
     public Sprite[] portrait_pool;
+
+    public Sprite Randomize()
+    {
+        return portrait_pool[Random.Range(0, portrait_pool.Length)];
+    }
 }
--- Assets/Scripts/Core/StudentGenerator.cs
+++ Assets/Scripts/Core/StudentGenerator.cs
@@ -1,47 +1,62 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public static class StudentGenerator {
-    public static Student Generate() {
+public static class StudentGenerator
+{
+    public static Student Generate(Portraits port)
+    {
         Student s = new Student();
 
         string first = Names.firstNames[Random.Range(0, Names.firstNames.Count)];
         string last = Names.lastNames[Random.Range(0, Names.lastNames.Count)];
         s.name = first + " " + last;
 
-        portrait = portrait_pool[Random.Range(0, portrait_pool.Count)];
+        s.portrait = port.Randomize();
 
         int grt_comp_per = Random.Range(0, 101);
-        if (grt_comp_per <= 20) {
+        if (grt_comp_per <= 20)
+        {
             s.grt_comp = Random.Range(0, 11);
-        } else if (grt_comp_per <= 80) {
+        }
+        else if (grt_comp_per <= 80)
+        {
             s.grt_comp = Random.Range(11, 21);
-        } else {
+        }
+        else
+        {
             s.grt_comp = Random.Range(21, 26);
         }
 
         int grt_lit_per = Random.Range(0, 101);
-        if (grt_lit_per <= 20) {
+        if (grt_lit_per <= 20)
+        {
             s.grt_lit = Random.Range(0, 11);
-        } else if (grt_lit_per <= 80) {
+        }
+        else if (grt_lit_per <= 80)
+        {
             s.grt_lit = Random.Range(11, 21);
-        } else {
+        }
+        else
+        {
             s.grt_lit = Random.Range(21, 26);
         }
 
         int amount_of_activities = Random.Range(1, 4);
         HashSet<string> usedActivities = new HashSet<string>();
 
-        for (int i = 0; i < amount_of_activities; i++) {
+        for (int i = 0; i < amount_of_activities; i++)
+        {
             int list;
             int index;
 
-            do {
+            do
+            {
                 list = Random.Range(0, Activity.activites.Count);
                 index = Random.Range(1, Activity.activites[list].Count);
             } while (usedActivities.Contains(Activity.activites[list][index]));
 
-            Activity temp = new Activity { // this way, can use subject to color code or update specific scores
+            Activity temp = new Activity
+            { // this way, can use subject to color code or update specific scores
                 subject = Activity.activites[list][0],
                 activity_name = Activity.activites[list][index],
                 activity_score = Random.Range(0, 51)
@@ -50,7 +65,8 @@ public static class StudentGenerator {
             usedActivities.Add(temp.activity_name);
             s.extracurriculars.Add(temp);
 
-            switch (temp.subject) {
+            switch (temp.subject)
+            {
                 case "Athletics": // could also assign a color here
                     s.Athletics += temp.activity_score;
                     break;
@@ -70,22 +86,32 @@ public static class StudentGenerator {
         }
 
         int ret_per = Random.Range(0, 101);
-        if (ret_per <= 20) {
+        if (ret_per <= 20)
+        {
             s.retentionLikeliness = Random.Range(0, 51);
-        } else if (ret_per <= 70) {
+        }
+        else if (ret_per <= 70)
+        {
             s.retentionLikeliness = Random.Range(51, 76);
-        } else {
+        }
+        else
+        {
             s.retentionLikeliness = Random.Range(76, 101);
         }
 
         int fin_per = Random.Range(0, 101);
-        if (fin_per <= 60) {
+        if (fin_per <= 60)
+        {
             if (fin_per <= 5) s.financialContribution = Random.Range(-25, -19) * 1000;
             else if (fin_per <= 35) s.financialContribution = Random.Range(-19, -9) * 1000;
             else s.financialContribution = Random.Range(-9, 0) * 1000;
-        } else if (fin_per <= 70) {
+        }
+        else if (fin_per <= 70)
+        {
             s.financialContribution = 0;
-        } else {
+        }
+        else
+        {
             if (fin_per <= 85) s.financialContribution = Random.Range(1, 11) * 1000;
             else if (fin_per <= 95) s.financialContribution = Random.Range(11, 21) * 1000;
             else s.financialContribution = Random.Range(21, 31) * 1000;
